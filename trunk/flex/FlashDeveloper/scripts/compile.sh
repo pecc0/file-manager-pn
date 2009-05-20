68c659e2a46c3c97cf85359d74fef4660e74ded6
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-05-20 12:04:41 +0000

    attempt to generate air without flex builder

--- trunk/flex/FlashDeveloper/scripts/compile.sh
+++ trunk/flex/FlashDeveloper/scripts/compile.sh
@@ -3,3 +3,4 @@
 cd $(dirname $0)/../src
 #echo $(pwd)
 mxmlc -output ../../../server/www/secure/accountHandler.swf accountHandler.mxml
+mxmlc +configname=air -output ../bin/accountHandler.exe accountHandler.mxml
