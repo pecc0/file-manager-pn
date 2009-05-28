93ab8f94376146c406cd3c86fff9d637697d83e6
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-05-28 09:36:11 +0000

    fixed a nasty io error for IE+SSL+session (server side)

--- trunk/server/www/secure/index.php
+++ trunk/server/www/secure/index.php
@@ -3,8 +3,8 @@
 	//echo '$_REQUEST ';
 	//print_r($_SERVER);
 	//print_r($_ENV);
-	header("Cache-Control: no-store");
-	session_start();
+	//header("Cache-Control: no-store");
+	include 'securedHeader.php';
 	//php faila trqbva da e izvikan s absoliuten pyt (e.x. https://localhost/secure/index/php)
 	echo SID;
 	
