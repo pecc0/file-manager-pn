8821a61788316e4b8fff58d490b5344118a14232
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-05-28 14:57:10 +0000

    texts for the file handler menu

--- server/www/include/securedHeader.php
+++ server/www/include/securedHeader.php
@@ -1,4 +1,5 @@
 <?php
+	//the AIR sends "Mozilla/5.0 (Windows; U; en) AppleWebKit/420+ (KHTML, like Gecko) AdobeAIR/1.0"
 	if (ereg("MSIE" ,$_SERVER['HTTP_USER_AGENT'])) {
 		//in case of internet explorer-don't cahche the contents
 		session_cache_limiter('public');
