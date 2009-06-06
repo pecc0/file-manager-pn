cf0b2c274e1ae45c4f4293e66d5b734c914ce6e7
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-06-06 12:56:01 +0000

    script for file system management "new directory" functionality

---
+++ trunk/server/www/secure/listdir.php
@@ -0,0 +1,81 @@
+<?php
+include 'securedHeader.php';
+
+function checkPath() {
+	include 'constants.php';
+	
+	global $user, $path, $requestDir, $selectedFile;
+	
+	$user = $_SESSION['UNAME'];
+
+	if ($user == '') {
+		echo "error:userNotLogged";
+		die;
+	}
+
+	$requestDir = $_POST["dir"];
+
+	$path = $usersHome . '/' . $user . '/' . $requestDir;
+
+	if (!is_dir($path)) {
+		$selectedFile = basename($path);
+		$path = dirname($path);
+		$requestDir = str_replace("\\", "/", dirname($requestDir));
+	}
+	$path = realpath($path);
+
+	if ($path == "") {
+		echo "error:" . "fileNotFound(" . urlencode($requestDir) . ")";
+		die;
+	}
+	$userPath = realpath($usersHome . '/' . $user);
+	if (substr_compare($userPath, $path, 0, strlen($userPath)) != 0) {
+		echo "error:" . $userPath . "!=" . $path;
+		die;
+	}
+}
+
+function printDirListing() {
+
+	global $user, $path, $requestDir, $selectedFile;
+	
+	chdir($path);
+
+	include 'xmlUtils.php';
+
+	$doc = new DOMDocument();
+	// we want a nice output
+	$doc->formatOutput = true;
+
+	$root = $doc->createElement("DirListing");
+	$root = $doc->appendChild($root);
+	addAttribute($doc, $root, 'username', $user);
+	addAttribute($doc, $root, 'listingfor', $requestDir);
+	addAttribute($doc, $root, 'selected', $selectedFile);
+
+	$dirs = array_diff( scandir( $path ), Array( ".", ".." ) );
+
+	foreach( $dirs as $d ){
+		if (!ereg("^\..*$", $d)) {
+			if( is_dir($path."/".$d) ) {
+				$subnode = $doc->createElement("dir");
+				$emptyFile = $doc->createElement("file");
+				$subnode->appendChild($emptyFile);
+				addAttribute($doc, $emptyFile, "name", "loading...");
+				addAttribute($doc, $subnode, "isBranch", "true");
+				//$doc->createAttribute('username')
+			} else {
+				$subnode = $doc->createElement("file");
+			}
+			addAttribute($doc, $subnode, "name", $d);
+			$root->appendChild($subnode);
+		}
+	}
+
+	print $doc->saveXML(); 
+}
+if (!$suppressDirList) {
+	checkPath();
+	printDirListing();
+}
+?>
