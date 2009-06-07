ac2b7a1886d304b58396c66c961a0e387b0a268b
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-06-07 10:09:55 +0000

    Bug in deploy.xml: Files are overridden now Deleting files and directories

--- server/www/secure/fscommand.php
+++ server/www/secure/fscommand.php
@@ -1,12 +1,44 @@
 <?php
 $suppressDirList=true;
 include 'listdir.php';
+
+function delete_file($fname) {
+	if (is_dir($fname)) {
+		$dir_handle = opendir($fname);
+		if (!$dir_handle)
+			return false;
+		while($file = readdir($dir_handle)) {
+			if ($file != "." && $file != "..") {
+				if (!delete_file($fname . "/" . $file)) {
+					return false;
+				}
+			}
+		}
+		closedir($dir_handle);
+		rmdir($fname);
+	} else {
+		if (!@unlink($fname)) {
+			return false;
+		}
+	}
+	return true;
+}
+
 checkPath();
 chdir($path);
 if ($_POST["command"]=="mkdir") {
 	if (!mkdir($selectedFile)) {
-		echo "error:mkdirError";
+		echo "error:mkdirError(" . $selectedFile . ")";
 		die;
+	}
+} else if ($_POST["command"]=="delete") {
+	foreach(split(";", $_POST["parameter"]) as $file) {
+		if (strlen($file) > 0) {
+			if (!delete_file($file)) {
+				echo "error:deleteFile(" . $file . ")";
+				die;
+			}
+		}
 	}
 }
 printDirListing();
