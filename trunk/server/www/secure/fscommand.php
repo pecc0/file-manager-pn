7cc007bca5e1026c2908e56b232d5623ec87eb33
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-06-08 20:35:22 +0000

    moving single files

--- trunk/server/www/secure/fscommand.php
+++ trunk/server/www/secure/fscommand.php
@@ -26,6 +26,10 @@ function delete_file($fname) {
 
 function local_path_encoded($absolute) {
 	global $userPath;
+	if (substr_compare($userPath, $absolute, 0, strlen($userPath)) != 0) {
+		//the path is not absolute
+		return urlencode($absolute);
+	}
 	return urlencode(substr($absolute, strlen($userPath)));
 }
 
@@ -34,7 +38,8 @@ function copy_file($src, $dst) {
 	if (is_dir($src)) {
 		$dir_handle = opendir($src);
 		if (!$dir_handle)
-			return false;
+			return "readDir(" . local_path_encoded($src) . ")";
+			
 		if (!@mkdir($dst)) {
 			return "error:mkdirFailed(" . local_path_encoded($dst) . ")";
 		}
@@ -52,7 +57,7 @@ function copy_file($src, $dst) {
 			return "error:copyFailed(" . local_path_encoded($src) . "," . local_path_encoded($dst) . ")";;
 		}
 	}
-	return true;
+	return "";
 }
 
 function get_new_name($file) {
@@ -63,35 +68,42 @@ function get_new_name($file) {
 	return $file . "_copy" . $i;
 }
 
-checkPath();
-chdir($path);
-if ($_POST["command"]=="mkdir") {
-	if (!mkdir($selectedFile)) {
-		echo "error:mkdirError(" . urlencode($selectedFile) . ")";
-		die;
-	}
-} else if ($_POST["command"]=="delete") {
-	foreach(split(";", $_POST["parameter"]) as $file) {
-		if (strlen($file) > 0) {
-			if (!delete_file($file)) {
-				echo "error:deleteFile(" . urlencode($file) . ")";
-				die;
-			}
-		}
-	}
-} else if ($_POST["command"]=="copy") {
+function get_dest_dir($destDir) {
+	global $userPath, $usersHome, $user;
 	
-	$destDir = $usersHome . '/' . $user . '/' . $_POST["destDir"];
+	$destDir = $usersHome . '/' . $user . '/' . $destDir;
 	
 	$destDir = realpath($destDir);
 	if ($destDir == "") {
-		echo "error:" . "destNotFound(" . urlencode($destDir) . ")";
+		echo "error:" . "destNotFound(" . local_path_encoded($destDir) . ")";
 		die;
 	}
 	if (substr_compare($userPath, $destDir, 0, strlen($userPath)) != 0) {
 		echo "error:" . $userPath . "!=" . $destDir;
 		die;
 	}
+	return $destDir;
+}
+
+checkPath();
+chdir($path);
+if ($_POST["command"]=="mkdir") {
+	if (!@mkdir($selectedFile)) {
+		echo "error:mkdirError(" . local_path_encoded($selectedFile) . ")";
+		die;
+	}
+} else if ($_POST["command"]=="delete") {
+	foreach(split(";", $_POST["parameter"]) as $file) {
+		if (strlen($file) > 0) {
+			if (!@delete_file($file)) {
+				echo "error:deleteFile(" . local_path_encoded($file) . ")";
+				die;
+			}
+		}
+	}
+} else if ($_POST["command"]=="copy") {
+	
+	$destDir = get_dest_dir($_POST["destDir"]);
 	foreach(split(";", $_POST["files"]) as $file) {
 		if (strlen($file) > 0) {
 			$dstFile = $file;
@@ -105,6 +117,23 @@ if ($_POST["command"]=="mkdir") {
 			}
 		}
 	}
+} else if ($_POST["command"]=="move") {
+	$destDir = get_dest_dir($_POST["destDir"]);
+	$files = split(";", $_POST["files"]);
+	foreach($files as $file) {
+		if (strlen($file) > 0) {
+			if (count($files) == 2) {
+				$destFile = $destDir . "/" . basename($_POST["dest"]);
+			} else {
+				$destFile = $destDir . "/" . $file;
+			}
+			if (!@rename($file, $destFile)) {
+				echo "error:moveFailed(" . local_path_encoded($file) . "," . local_path_encoded($destFile) . ")";
+				die;
+			}
+		}
+	}
 }
+chdir($path);
 printDirListing();
 ?>
