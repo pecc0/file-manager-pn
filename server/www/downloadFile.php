6c3aef8501e2c0a98cde63df1f5a22e95190dad0
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-06-10 14:05:28 +0000

    Download file working

--- server/www/downloadFile.php
+++ server/www/downloadFile.php
@@ -1,18 +1,32 @@
 <?php
-//error_log(print_r($_FILES, true));
+error_log(print_r($_GET, true));
 session_start();
 
-//error_log(print_r($_POST, true));
+error_log(print_r($_SESSION, true));
+error_log(session_id());
 if ($_SESSION['command']=='download') {
 	//error_log('file ok');
 	//error_log(print_r($_FILES, true));
 	//error_log($_SESSION['userDir'] . '/' . $_POST['serverfile']);
 	
+	
+	/*
+	$tempdir = $tempDirectory . '/' . session_id();
+	delete_file($tempdir);
+	mkdir($tempdir);
+	if ($_SESSION['parameter'] == "<raw_post_data>") {
+	} else {
+		$myFile = $_SESSION['userDir'] . '/' . $_SESSION['parameter'];
+		error_log('$myFile=' . $myFile);
+		copy($myFile, $tempdir . '/' . basename($myFile));
+		echo '/tmp/' . session_id() . '/' . basename($myFile);
+	}
+	*/
+	
 	$myFile = $_SESSION['userDir'] . '/' . $_SESSION['parameter'];
-	error_log($myFile);
 	$fh = @fopen($myFile, 'r');
 	if (!$fh) {
-		echo "failed to open file";
+		echo "Failed to open file";
 		die;
 	}
 	$theData = fread($fh, filesize($myFile));
@@ -22,6 +36,7 @@ if ($_SESSION['command']=='download') {
 	header("Content-Disposition: attachment; filename=".basename(realpath($myFile)));
 	echo $theData;
 	error_log("sent");
+	
 	session_unset();
 	session_destroy();
 }
--- server/www/include/constants.php
+++ server/www/include/constants.php
@@ -2,5 +2,5 @@
 $shmTextsID=1;
 $usersHome=$_SERVER['DOCUMENT_ROOT'] . "/home/";
 $confXml=".conf.xml";
-
+$tempDirectory=$_SERVER['DOCUMENT_ROOT'] . "/tmp/";
 ?>
