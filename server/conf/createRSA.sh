5b9857be52c94679608c5c201538a2c17220683a
air_biker@abv.bg <air_biker@abv.bg@bcd0efce-284c-11de-8780-0390ca4c1f59>
2009-07-09 09:54:06 +0000

    sertificate fixes

--- server/conf/createRSA.sh
+++ server/conf/createRSA.sh
@@ -1,6 +1,7 @@
 #!bash
 openssl genrsa -des3 -out my-ca.key 2048
 openssl req -new -x509 -days 3650 -key my-ca.key -out my-ca.crt
+echo !!!Watch out: Common Name must be the server name
 openssl genrsa -des3 -out mars-server.key 1024
 openssl req -new -key mars-server.key -out mars-server.csr
 openssl x509 -req -in mars-server.csr -out mars-server.crt -sha1 -CA my-ca.crt -CAkey my-ca.key -CAcreateserial -days 3650
